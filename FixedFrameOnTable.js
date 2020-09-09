2394b624f458b0f3e9318d554c8859d28bb4f00e
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-09-09 15:51:31 +0900

    자동변환

--- FixedFrameOnTable.js
+++ FixedFrameOnTable.js
@@ -24,10 +24,10 @@ FixedFrameOnTable.prototype.init = function(ffot){
 	this.content = ffot.querySelector('.ffot-content');
 	this.useSyncScrollTop = !!ffot.querySelector('.ffot-container-left .ffot-header-10');
 
-	this.initColgroup(this.content);
-	this.initColgroup(this.header00);
-	this.initColgroup(this.header01);
-	this.initColgroup(this.header10);
+	FixedFrameOnTable.initColgroup(this.content);
+	FixedFrameOnTable.initColgroup(this.header00);
+	FixedFrameOnTable.initColgroup(this.header01);
+	FixedFrameOnTable.initColgroup(this.header10);
 
 
 	this.initEvent();
@@ -60,33 +60,7 @@ FixedFrameOnTable.prototype.initEvent = function(){
 	}
 
 }
-FixedFrameOnTable.prototype.initColgroup = function(tbl){ //테이블에 colgroup 이 있는지 체크하고 없으면, 만든다.
-	if(!tbl || tbl.nodeName!='TABLE'){ return false;}
-	var cols = tbl.querySelectorAll('col');
-	if(cols.length>0){return true;}
-	let max_count_td = 0;
-	let max_tr = null,tds = null;
-	tbl.querySelectorAll('tr').forEach((tr, i) => {
-		tds = tr.querySelectorAll('td,th')
-		if(max_count_td < tds.length){
-			max_tr = tr;
-		}
-		max_count_td = Math.max(max_count_td,tds.length);
-	});
-	;
-	var colgroup = document.createElement('colgroup');
-	let i=0,col=null,rect=null;
-	let max_tds = max_tr.querySelectorAll('td,th');
-	while(i < max_count_td){
-		col = document.createElement('col');
-		rect = max_tds[i].getBoundingClientRect();
-		col.width =rect.width;
-		colgroup.append(col);
-		i++;
-	}
-	tbl.append(colgroup)
-	return max_count_td;
-}
+
 FixedFrameOnTable.prototype.sync = function(){
 	this.syncSizeHeader00();
 	this.syncSizeContent();
@@ -143,10 +117,13 @@ FixedFrameOnTable.prototype.syncSizeContent = function(){
 	if(this.header10){
 		this.header10.style.height = rect_fbc.height+"px";
 		let height_tds = this.content.querySelectorAll('tr > *:first-child')
-		this.header10.querySelectorAll('tr > *:first-child').forEach((item, i) => {
-			let rect = height_tds[i].getBoundingClientRect();
-			item.height = rect.height;
-		});
+		if(height_tds.length>0){
+			this.header10.querySelectorAll('tr > *:first-child').forEach((item, i) => {
+				let rect = height_tds[i].getBoundingClientRect();
+				item.height = rect.height;
+			});
+		}
+
 	}
 
 	if(this.header01){
@@ -162,4 +139,156 @@ FixedFrameOnTable.prototype.syncScrollTop = function(ta){
 	// let ta = evt.target;
 	let el = this.header10;
 	el.style.transform="translateY(-"+ta.scrollTop+"px)";
+}
+
+//--
+FixedFrameOnTable.initColgroup = function(tbl){ //테이블에 colgroup 이 있는지 체크하고 없으면, 만든다.
+	if(!tbl || tbl.nodeName!='TABLE'){ return false;}
+	var cols = tbl.querySelectorAll('col');
+	if(cols.length>0){return true;}
+	let max_count_td = 0;
+	let max_tr = null,tds = null;
+	tbl.querySelectorAll('tr').forEach((tr, i) => {
+		tds = tr.querySelectorAll('td,th')
+		if(max_count_td < tds.length){
+			max_tr = tr;
+		}
+		max_count_td = Math.max(max_count_td,tds.length);
+	});
+	;
+	var colgroup = document.createElement('colgroup');
+	let i=0,col=null,rect=null;
+	let max_tds = max_tr?max_tr.querySelectorAll('td,th'):null;
+	while(max_tds && i < max_count_td){
+		col = document.createElement('col');
+		rect = max_tds[i].getBoundingClientRect();
+		col.width =rect.width;
+		colgroup.append(col);
+		i++;
+	}
+	tbl.append(colgroup)
+	return max_count_td;
+}
+//--
+FixedFrameOnTable.tableToFfot = function(ffotNode,table,rowCount,colCount){
+	// let table = tableNode.cloneNode(true)
+	FixedFrameOnTable.initColgroup(table);
+	console.log(table);
+	ffotNode.classList.add('ffot');
+	let scrollbar = document.createElement('div');
+	scrollbar.className = 'ffot-scrollbar';
+	ffotNode.append(scrollbar)
+	let containerLeft = document.createElement('div');
+	containerLeft.className = 'ffot-container-left';
+	scrollbar.append(containerLeft)
+	let containerBody= document.createElement('div');
+	containerBody.className = 'ffot-container-body';
+	scrollbar.append(containerBody)
+
+	let clonedTable = table.cloneNode(false);
+	clonedTable.removeAttribute('id');
+	clonedTable.append(document.createElement('colgroup'));
+
+	let header00 = null,header10=null,header01=null;
+	if(rowCount>0 && colCount > 0){
+		header00 = clonedTable.cloneNode(true);
+		header00.createTBody()
+		header00.classList.add('ffot-header-00');
+		containerLeft.append(header00)
+	}
+	if(colCount > 0){
+
+		header10 = clonedTable.cloneNode(true);
+		header10.createTBody()
+		header10.classList.add('ffot-header-10');
+		if(rowCount==0){
+			containerBody.classList.add('ffot-container-body-dir-row');
+			containerBody.append(header10)
+		}else{
+			containerLeft.append(header10)
+		}
+
+	}
+	if(rowCount > 0){
+		header01 = clonedTable.cloneNode(true);
+		header01.createTBody()
+		header01.classList.add('ffot-header-01');
+		containerBody.append(header01)
+	}
+	let content= clonedTable.cloneNode(true);
+	content.createTBody()
+	content.classList.add('ffot-content');
+	containerBody.append(content)
+
+
+	let trs = table.rows;
+	if(trs.length<rowCount){
+		console.error("테이블에서 가져올 수 있는 rowCount가 넘었습니다.");
+		return false;
+	}
+	let tr = null , cells = null , cell=null, ttr=null;
+	for(var i=0,m=rowCount;i<m;i++){ //상단 처리
+		tr = trs[i];
+		cells = tr.cells
+		ttr = tr.cloneNode(false)
+
+		for(var i2=0,m2=colCount;i2<m2;i2++){ //header00
+			ttr.append(cells[i2].cloneNode(true))
+		}
+		if(header00){
+			header00.tBodies[0].append(ttr)
+		}
+		ttr = tr.cloneNode(false)
+		for(i2=m2,m2=cells.length;i2<m2;i2++){ //header01
+			ttr.append(cells[i2].cloneNode(true))
+		}
+		if(header01){
+			header01.tBodies[0].append(ttr)
+		}
+	}
+
+	for(i=m,m=trs.length;i<m;i++){ //하단 처리
+		tr = trs[i];
+		cells = tr.cells
+		ttr = tr.cloneNode(false)
+		for(var i2=0,m2=colCount;i2<m2;i2++){ //header10
+			ttr.append(cells[i2].cloneNode(true))
+		}
+		if(header10){
+			header10.tBodies[0].append(ttr)
+		}
+		ttr = tr.cloneNode(false)
+		for(i2=m2,m2=cells.length;i2<m2;i2++){ //content
+			ttr.append(cells[i2].cloneNode(true))
+		}
+		if(content){
+			content.tBodies[0].append(ttr)
+		}
+	}
+	//-- colgroup 복사
+	let cols = table.querySelectorAll('col'),colgroup=null;
+	for(var i2=0,m2=colCount;i2<m2;i2++){ //header10
+		if(header00) header00.querySelector('colgroup').append(cols[i2].cloneNode(true));
+		if(header10) header10.querySelector('colgroup').append(cols[i2].cloneNode(true));
+	}
+	for(m2=cols.length;i2<m2;i2++){ //content
+		if(header01) header01.querySelector('colgroup').append(cols[i2].cloneNode(true));
+		if(content) content.querySelector('colgroup').append(cols[i2].cloneNode(true));
+	}
+
+
+	// for(let i=0,m=trs.length;i<m;i++){
+	// 	tr = trs[i];
+	// 	for(var i2=0,m2=colCount;i2<m2;i++){ //header10
+	// 		// htrs.push(trs[i])
+	// 	}
+	// 	for(i2=m2,m2=tr.cells.length;i2<m2;i++){ //content
+	// 		// htrs.push(trs[i])
+	// 	}
+	// }
+	// console.log(htrs);
+	// let h00trs = []
+
+	return new FixedFrameOnTable(ffotNode);
+
 }
